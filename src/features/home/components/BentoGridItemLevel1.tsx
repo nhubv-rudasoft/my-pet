ed0543970a1e94cffdab49605b0d2180d26b0fe9
nhubv-rudasoft <169869374+nhubv-rudasoft@users.noreply.github.com>
2025-02-14 14:06:19 +0700

    responsive error

--- src/features/home/components/BentoGridItemLevel1.tsx
+++ src/features/home/components/BentoGridItemLevel1.tsx
@@ -9,27 +9,25 @@ interface BentoGridItemLevel1Props {
 export const BentoGridItemLevel1 = ({petItem}: BentoGridItemLevel1Props) => {
   return (
     <div className="col-span-2 row-span-2 bg-white rounded-xl p-4 sm:p-6 shadow-md">
-      <div className="flex flex-col h-full">
-        <div className="flex items-center gap-4">
-          <img
-            src={petPhoto}
-            alt={petItem.name}
-            className="object-contain"
-            style={{width: 300, height: 'auto'}}
-          />
-          <div className={'flex flex-col gap-2'}>
-            <div>
-              <h2 className="text-lg sm:text-2xl font-semibold">
-                {petItem.name} 1
-              </h2>
-              <p className="text-xs sm:text-sm text-muted-foreground">
-                {petItem.description}
-              </p>
-            </div>
-            <p className="text-sm sm:text-xl font-semibold text-red-900 mt-1">
-              {formatPrice(petItem.price)}
+      <div className="flex flex-col md:flex-row lg:flex-row items-center gap-4">
+        <img
+          src={petPhoto}
+          alt={petItem.name}
+          className="object-contain"
+          style={{width: 300, height: 'auto'}}
+        />
+        <div className={'flex flex-col gap-2'}>
+          <div>
+            <h2 className="text-lg sm:text-2xl font-semibold">
+              {petItem.name} 1
+            </h2>
+            <p className="text-xs sm:text-sm text-muted-foreground">
+              {petItem.description}
             </p>
           </div>
+          <p className="text-sm sm:text-xl font-semibold text-red-900 mt-1">
+            {formatPrice(petItem.price)}
+          </p>
         </div>
       </div>
     </div>
